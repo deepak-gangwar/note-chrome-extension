b9567fceb8fca91c907087bf3bb9fabd8716149a
Deepak <deepakgangwar4265@gmail.com>
2023-04-24 19:32:06 +0530

    Successfully inject react app into webpage on extension click

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const app = document.createElement('div')
+app.setAttribute('id', 'extension-app')
+document.body.appendChild(app)
+
+// ReactDOM.createRoot(document.createElement('div')).render(
+ReactDOM.createRoot(document.getElementById('extension-app')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
