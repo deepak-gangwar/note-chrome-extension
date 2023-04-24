20234e020bdbc360ac17c72bc070d617c8efe359
Deepak <deepakgangwar4265@gmail.com>
2023-04-24 13:58:22 +0530

    Chrome extension setup, update config and add background and content script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        "popup": path.resolve('src/popup'),
+        // options: path.resolve('src/options/options.tsx'),
+        "background": path.resolve('src/background'),
+        "content-script": path.resolve('src/content-script'),
+        // newTab: path.resolve('src/tabs/index.tsx')
+      },
+      output: {
+        entryFileNames: '[name].js',
+      }
+    },
+
+    // watch: {
+    // include: 'src/**'
+    // https://rollupjs.org/configuration-options/#watch
+    // },
+  },
+
+  server:
+  {
+    https: true,
+    open: true,
+    port: 8080,
+  },
+
+  // copies all files from this directory as-is
+  publicDir: './src/static',
+  plugins: [
+    react(),
+
+    // this is for HTTPS connection
+    mkcert(),
+  ],
 })
