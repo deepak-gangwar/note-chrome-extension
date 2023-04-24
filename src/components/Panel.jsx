b15e442c28fe41bcb5665a7185046b9c4220ebe0
Deepak <deepakgangwar4265@gmail.com>
2023-04-24 16:54:33 +0530

    Add basic styles for search component

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,7 +1,10 @@
 import styles from '../styles/Panel.module.css'
+import Search from './Search'
 
 export default function Panel() {
     return (
-        <div className={styles.panel}></div>
+        <div className={styles.panel}>
+            <Search />
+        </div>
     )
 }
