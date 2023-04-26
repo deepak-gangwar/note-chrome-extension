4a88a9cee324bce495c86e6aa9539ee01ec0e4ba
Deepak <deepakgangwar4265@gmail.com>
2023-04-26 22:36:25 +0530

    Add final styles for the export menu

--- src/components/AddExportToolbar/Export/ExportMenu/styles.js
+++ src/components/AddExportToolbar/Export/ExportMenu/styles.js
@@ -1,6 +1,36 @@
 export default {
-    export_btn: {},
-    exporrt_btn_expand: {
-        
+    export_menu: {
+        background: "#232323",
+        padding: "1.2rem 1.5rem",
+        position: "absolute",
+        width: "max-content",
+        listStyleType: "none",
+        bottom: "100%",
+        right: "9px",
+        borderRadius: "10px",
+    },
+
+    menu_item: {
+        textDecoration: "none",
+        color: "#f5f5f5",
+        borderBottom: "1px solid #f5f5f540",
+        padding: "6px 0"
+    },
+
+    menu_item_a: {
+        fontWeight: "normal",
+        textDecoration: "none",
+        color: "#f5f5f5"
+    },
+
+    pin: {
+        width: "fit-content",
+        position: "absolute",
+        top: "94%",
+        left: "80%",
+    },
+
+    pin_svg: {
+        transform: "scale(0.6)",
     }
 }
