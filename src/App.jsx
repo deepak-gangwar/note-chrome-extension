790ab6277ae9529840e24d19d596e16eef95cf96
Deepak <deepakgangwar4265@gmail.com>
2023-04-26 23:16:30 +0530

    feat: text selection basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './styles/App.css'
 import Panel from './components/Panel'
+import Tooltip from './components/Tooltip'
 
 function App() {
     return (
         <>
+            <Tooltip />
             <Panel />
         </>
     )
---
+++ src/components/Tooltip/styles.js
@@ -0,0 +1,10 @@
+export default {
+    tooltip: {
+        background: "coral",
+        color: "#f5f5f5",
+        position: "absolute",
+        bottom: "0",
+        left: "0",
+        borderRadius: "0",
+    }
+}
